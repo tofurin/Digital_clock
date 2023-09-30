8b20418fbb7c587b3f8bab82c26d29f5854ece48
tofurin <prime65573@protonmail.com>
2023-09-30 20:09:52 +0900

    basic function

---
+++ app/src/main/java/com/example/digitalclock/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.digitalclock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView Texttime;
+
+    Timer timer;
+
+    Handler handler = new Handler();
+
+    //Button setting_button;
+    //Button exit_btn;
+
+    View setting_layout;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setStatusBarColor(Color.BLACK);
+
+        //各buttonのid取得
+        Texttime = findViewById(R.id.Texttime);
+        //setting_button = findViewById(R.id.setting_button);
+        //setting_layout = findViewById(R.id.setting_layout);
+        //exit_btn = findViewById(R.id.exit_btn);
+
+        //各buttonのリスナークラス登録
+        //setting_button.setOnClickListener(new setting_button_listener());
+        //exit_btn.setOnClickListener(new exit_btn_listener());
+
+        timer = new Timer();
+
+        timer.scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run() {
+                //現在時刻を取得
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+                final String currenttime = sdf.format(new Date());
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Texttime.setText(currenttime);
+                    }
+
+                });
+            }
+        }, 0, 1000);
+    }
+
+    //public class setting_button_listener implements View.OnClickListener{
+        //@Override
+        //public void onClick(View view){
+            //setting_layout.setVisibility(View.VISIBLE);
+        //}
+    //}
+
+    //public class exit_btn_listener implements View.OnClickListener{
+        //@Override
+        //public void onClick(View view){
+            //setting_layout.setVisibility(View.GONE);
+        //}
+    //}
+}
